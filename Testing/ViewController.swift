3fe2d5a2ae063c28935d0d84374dc214616e6d31
fikrihelmi17 <fikrihelmi17@gmail.com>
2020-07-22 13:54:11 +0700

    Initial commit

---
+++ Testing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Testing
+//
+//  Created by Fikri on 21/07/20.
+//  Copyright © 2020 Fikri Helmi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
